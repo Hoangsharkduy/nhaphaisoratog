2933d118a3b5394c3e2b7de0d1ffcd165af3ff9f
ADMIN <ADMIN@LAB1-MAY30>
2024-02-26 08:13:31 +0700

    Add project files.

---
+++ nhaphaisoratog/nhaphaisoratog.cpp
@@ -0,0 +1,25 @@
+// nhaphaisoratog.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int so1, so2, tong;
+    cout << "CHUONG TRINH TINH TONG\n";
+    cout << "Nhap so thu nhat:";
+    cin >> so1;
+    cout << "Nhap so thu hai:";
+    cin >> so2;
+    tong = so1 + so2;
+    cout << so1 << "+" << so2 << "="
+        << tong
+        << endl;
+    return 0; 
+
+}
+
+    
+    
+
+
+
